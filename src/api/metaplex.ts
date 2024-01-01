3ecb709e156980a680c9d91b27a1c069500783e1
cmooredev <cmooredev@pm.me>
2023-12-31 20:02:43 -0800

    more logs

--- src/api/metaplex.ts
+++ src/api/metaplex.ts
@@ -3,10 +3,12 @@ import { PublicKey, Connection } from '@solana/web3.js';
 
 export const fetchUserNFTs = async (publicKey: string, connection: Connection) => {
     const metaplex = new Metaplex(connection);
+    console.log("metaplex", metaplex);
     const input: FindNftsByOwnerInput = {
         owner: new PublicKey(publicKey),
         // add other required properties here
     };
     const ownerNFTs = await metaplex.nfts().findAllByOwner(input);
+    console.log("owner nfts", ownerNFTs);
     return ownerNFTs;
 };
